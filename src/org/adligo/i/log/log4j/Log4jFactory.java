247b6c7080b5310b9d2dda493bb2fa130be790be
scott <scott@adligo.com>
2009-10-07 23:22:09 +0000

    updated apis

--- src/org/adligo/i/log/log4j/Log4jFactory.java
+++ src/org/adligo/i/log/log4j/Log4jFactory.java
@@ -5,14 +5,16 @@ import java.util.Map;
 
 import org.adligo.i.log.client.I_LogDelegate;
 import org.adligo.i.log.client.I_LogFactory;
-import org.adligo.i.log.client.I_ProxyLog;
-import org.adligo.i.log.client.models.I_LogMessage;
 import org.adligo.i.util.client.ClassUtils;
-import org.adligo.i.util.client.I_Collection;
-import org.adligo.i.util.client.I_Iterator;
 import org.adligo.i.util.client.I_Map;
 
 public class Log4jFactory implements I_LogFactory {
+	/**
+	 * convention for logFactory=org.adligo.i.log.log4j.Log4jFactory
+	 * setting in adligo_log.properties
+	 */
+	public static final String LOG_FACTORY_NAME = Log4jFactory.class.getName();
+	
 	private static final Map<String, I_LogDelegate> logs = new HashMap<String, I_LogDelegate>();
 	public static final Log4jFactory INSTANCE = new Log4jFactory();
 	
@@ -43,33 +45,6 @@ public class Log4jFactory implements I_LogFactory {
 	@Override
 	public void resetLogLevels() {
 		throw new RuntimeException("Method not yet implemented. ");
-	}
-
-	@Override
-	public void sendPreInitMessages(I_Collection iLogMessages) {
-		I_Iterator it = iLogMessages.getIterator();
-		while (it.hasNext()) {
-			I_LogMessage ilog = (I_LogMessage) it.next();
-			if (ilog != null) {
-				String name = ilog.getName();
-				I_LogDelegate logDelegate = this.getLog(name);
-				logDelegate.log(ilog.getLevel(), 
-						ilog.getMessage(),
-						ilog.getThrowable());
-			}
-		}
-	}
-
-	@Override
-	public synchronized void setInitalLogLevels(I_Collection iProxyLogs) {
-		//do nothing this expects to be staticly init 
-		/*
-		I_Iterator it = iProxyLogs.getIterator();
-		while (it.hasNext()) {
-			I_ProxyLog proxyLog = (I_ProxyLog) it.next();
-			proxyLog.addDelegate(new Log4jLog(proxyLog.getLogName()));
-		}
-		*/
 	}
 
 	@Override
