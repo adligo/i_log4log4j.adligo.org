47da80e68650689c3cc9b1b626fa317bd5cbd907
scott <scott@adligo.com>
2009-10-07 04:26:12 +0000

    i want my log4j

---
+++ src/org/adligo/i/log/log4j/Log4jFactory.java
@@ -0,0 +1,81 @@
+package org.adligo.i.log.log4j;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.adligo.i.log.client.I_LogDelegate;
+import org.adligo.i.log.client.I_LogFactory;
+import org.adligo.i.log.client.I_ProxyLog;
+import org.adligo.i.log.client.models.I_LogMessage;
+import org.adligo.i.util.client.ClassUtils;
+import org.adligo.i.util.client.I_Collection;
+import org.adligo.i.util.client.I_Iterator;
+import org.adligo.i.util.client.I_Map;
+
+public class Log4jFactory implements I_LogFactory {
+	private static final Map<String, I_LogDelegate> logs = new HashMap<String, I_LogDelegate>();
+	public static final Log4jFactory INSTANCE = new Log4jFactory();
+	
+	private Log4jFactory() {}
+	
+	@SuppressWarnings("unchecked")
+	@Override
+	public synchronized I_LogDelegate getLog(Class clazz) {
+		return getLogInternal(ClassUtils.getClassName(clazz));
+	}
+
+	@Override
+	public I_LogDelegate getLog(String name) {
+		return getLogInternal(name);
+	}
+
+	private synchronized I_LogDelegate getLogInternal(String name) {
+		I_LogDelegate del = logs.get(name);
+		if (del != null) {
+			return del;
+		}
+		
+		I_LogDelegate delegate = new Log4jLog(name);
+		
+		return delegate;
+	}
+
+	@Override
+	public void resetLogLevels() {
+		throw new RuntimeException("Method not yet implemented. ");
+	}
+
+	@Override
+	public void sendPreInitMessages(I_Collection iLogMessages) {
+		I_Iterator it = iLogMessages.getIterator();
+		while (it.hasNext()) {
+			I_LogMessage ilog = (I_LogMessage) it.next();
+			if (ilog != null) {
+				String name = ilog.getName();
+				I_LogDelegate logDelegate = this.getLog(name);
+				logDelegate.log(ilog.getLevel(), 
+						ilog.getMessage(),
+						ilog.getThrowable());
+			}
+		}
+	}
+
+	@Override
+	public synchronized void setInitalLogLevels(I_Collection iProxyLogs) {
+		I_Iterator it = iProxyLogs.getIterator();
+		while (it.hasNext()) {
+			I_ProxyLog proxyLog = (I_ProxyLog) it.next();
+			proxyLog.addDelegate(new Log4jLog(proxyLog.getLogName()));
+		}
+	}
+
+	@Override
+	public void setInitalLogLevels(I_Map props, I_LogFactory p) {
+		//do nothing this is for async log config file responses like adligo_log.properties
+	}
+
+	@Override
+	public boolean isStaticInit() {
+		return true;
+	}
+}
---
+++ src/org/adligo/i/log/log4j/Log4jLog.java
@@ -0,0 +1,267 @@
+package org.adligo.i.log.log4j;
+
+import org.adligo.i.log.client.I_LogDelegate;
+import org.adligo.i.log.client.Log;
+import org.adligo.i.log.client.SimpleLog;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+
+public class Log4jLog implements Log, I_LogDelegate {
+	private boolean enabled = true;
+	private Logger logger;
+	
+	public Log4jLog(String logName) {
+		logger = Logger.getLogger(logName);
+	}
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return enabled;
+	}
+
+	@Override
+	public boolean isErrorEnabled() {
+		if (enabled) {
+			return logger.isEnabledFor(Level.ERROR);
+		}
+		return false;
+	}
+
+	@Override
+	public boolean isFatalEnabled() {
+		if (enabled) {
+			return logger.isEnabledFor(Level.FATAL);
+		}
+		return false;
+	}
+
+	@Override
+	public boolean isWarnEnabled() {
+		if (enabled) {
+			return logger.isEnabledFor(Level.WARN);
+		}
+		return false;
+	}
+
+    /**
+     * @Override
+     */
+    public boolean isDebugEnabled() {
+    	if (enabled) {
+    		return logger.isEnabledFor(Level.DEBUG);
+    	}
+    	return false;
+    }
+
+
+    /**
+     * @Override
+     */
+    public boolean isInfoEnabled() {
+    	if (enabled) {
+    		return logger.isEnabledFor(Level.INFO);
+    	}
+    	return false;
+    }
+
+
+    /**
+     * @Override
+     */
+    public boolean isTraceEnabled() {
+    	if (enabled) {
+    		return logger.isEnabledFor(Level.TRACE);
+    	}
+    	return false;
+    }
+    
+	@Override
+	public void setEnabled(boolean p) {
+		enabled = p;
+	}
+
+	@Override
+	public void log(short type, Object message, Throwable t) {
+		if (enabled) {
+			switch (type) {
+				case SimpleLog.LOG_LEVEL_TRACE:
+						if (message != null) {
+							if (t != null) {
+								trace(message, t);
+							} else {
+								trace(message);
+							}
+						}
+					break;
+				case SimpleLog.LOG_LEVEL_DEBUG:
+					if (message != null) {
+						if (t != null) {
+							debug(message, t);
+						} else {
+							debug(message);
+						}
+					}
+					break;
+				case SimpleLog.LOG_LEVEL_INFO:
+					if (message != null) {
+						if (t != null) {
+							info(message, t);
+						} else {
+							info(message);
+						}
+					}
+					break;
+				case SimpleLog.LOG_LEVEL_WARN:
+					if (message != null) {
+						if (t != null) {
+							warn(message, t);
+						} else {
+							warn(message);
+						}
+					}
+					break;
+				case SimpleLog.LOG_LEVEL_ERROR:
+					if (message != null) {
+						if (t != null) {
+							error(message, t);
+						} else {
+							error(message);
+						}
+					}
+					break;
+				case SimpleLog.LOG_LEVEL_FATAL:
+					if (message != null) {
+						if (t != null) {
+							fatal(message, t);
+						} else {
+							fatal(message);
+						}
+					}
+					break;
+			}
+		}
+	}
+
+	@Override
+	public void debug(Object message) {
+		logger.debug(message);
+	}
+
+	@Override
+	public void debug(Object message, Throwable t) {
+		logger.debug(message, t);
+	}
+
+	@Override
+	public void error(Object message) {
+		logger.error(message);
+	}
+
+	@Override
+	public void error(Object message, Throwable t) {
+		logger.error(message, t);
+	}
+
+	@Override
+	public void fatal(Object message) {
+		logger.fatal(message);
+	}
+
+	@Override
+	public void fatal(Object message, Throwable t) {
+		logger.fatal(message, t);
+	}
+
+	@Override
+	public void info(Object message) {
+		logger.info(message);
+	}
+
+	@Override
+	public void info(Object message, Throwable t) {
+		logger.info(message, t);
+	}
+
+	@Override
+	public void trace(Object message) {
+		logger.trace(message);
+	}
+
+	@Override
+	public void trace(Object message, Throwable t) {
+		logger.trace(message, t);
+	}
+
+	@Override
+	public void warn(Object message) {
+		logger.warn(message);
+	}
+
+	@Override
+	public void warn(Object message, Throwable t) {
+		logger.warn(message, t);
+	}
+	
+/*
+	public void debug(String message) {
+		logger.debug(message+ "\n");
+	}
+
+	
+	public void debug(String message, Throwable t) {
+		logger.debug(message+ "\n", t);
+	}
+
+	
+	public void error(String message) {
+		logger.error(message+ "\n");
+	}
+
+	
+	public void error(String message, Throwable t) {
+		logger.error(message+ "\n", t);
+	}
+
+	
+	public void fatal(String message) {
+		logger.fatal(message+ "\n");
+	}
+
+	
+	public void fatal(String message, Throwable t) {
+		logger.fatal(message+ "\n", t);
+	}
+
+	
+	public void info(String message) {
+		logger.info(message+ "\n");
+	}
+
+	
+	public void info(String message, Throwable t) {
+		logger.info(message+ "\n", t);
+	}
+
+	
+	public void trace(String message) {
+		logger.trace(message+ "\n");
+	}
+
+	
+	public void trace(String message, Throwable t) {
+		logger.trace(message+ "\n", t);
+	}
+
+	
+	public void warn(String message) {
+		//line feed for eclipse
+		logger.warn(message + "\n");
+	}
+
+	
+	public void warn(String message, Throwable t) {
+		logger.warn(message + "\n", t);
+	}
+	*/
+}
